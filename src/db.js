c3e85909a77a7cb38de54f2f8b6e29754281f118
Ibhan <ik1025@nyu.edu>
2017-11-09 18:10:43 -0500

    nothing

--- src/db.js
+++ src/db.js
@@ -18,7 +18,7 @@ const Tournament = new mongoose.Schema({
 
 mongoose.model('Tournament',Tournament);
 
-
+let dbconf;
 // is the environment variable, NODE_ENV, set to PRODUCTION? 
 if (process.env.NODE_ENV === 'PRODUCTION') {
  // if we're in PRODUCTION mode, then read the configration from a file
@@ -31,10 +31,16 @@ if (process.env.NODE_ENV === 'PRODUCTION') {
  // our configuration file will be in json, so parse it and set the
  // conenction string appropriately!
  const conf = JSON.parse(data);
- let dbconf = conf.dbconf;
+
+  dbconf = conf.dbconf;
 } else {
  // if we're not in PRODUCTION mode, then use
  dbconf = 'mongodb://localhost/ik1025';
 }
 
 mongoose.connect(dbconf);
+
+// {"dbconf":"mongodb://ik1025:XDsd5mZy@class-mongodb.cims.nyu.edu/ik1025"}
+
+
+// PORT=14433 NODE_ENV=PRODUCTION node app.js
